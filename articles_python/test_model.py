4218105a87c58a7ca78c22a57cf3f04499329690
kevinleemiguel <iradukundaleekevin@gmail.com>
2020-06-11 10:42:57 +0200

    fail test on purpose

--- articles_python/test_model.py
+++ articles_python/test_model.py
@@ -8,7 +8,7 @@ class TestModels(TestCase):
         article = Article()
         article.title = 'hello'
         article.description = 'Description'
-        expected_object_name = article.title
+        expected_object_name = article.description
         self.assertEquals(expected_object_name, str(article))
 
         print('1. MODELS Testing toString article Passed!...')

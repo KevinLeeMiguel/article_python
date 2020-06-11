cc2906c9528447b1f7340f925c31d5b805e0412c
kevinleemiguel <iradukundaleekevin@gmail.com>
2020-06-11 11:20:01 +0200

    add failing test

--- articles_python/test_model.py
+++ articles_python/test_model.py
@@ -12,3 +12,12 @@ class TestModels(TestCase):
         self.assertEquals(expected_object_name, str(article))
 
         print('1. MODELS Testing toString article Passed!...')
+
+    def test_str(self):
+        article = Article()
+        article.title = 'hello'
+        article.description = 'Description'
+        expected_object_name = article.description
+        self.assertEquals(expected_object_name, str(article))
+
+        print('1. MODELS Testing toString article Passed!...')
